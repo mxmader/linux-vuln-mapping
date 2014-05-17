7edc21c4ee770c263c3802cb2c3836024e6d00c9
Joe Mader <jmader@jmader.com>
2014-05-17 10:24:46 -0400

    new nist downloader

--- download_nist_cves.py
+++ download_nist_cves.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python2
 
-print "not yet implemented"
+from BeautifulSoup import BeautifulSoup as bsoup
+
+import os
+import posixpath
+import re
+import requests
+import urlparse
+
+download_chunk_size = 131072
+download_dir = "nist_cves"
+
+if not os.path.exists(download_dir):
+	print "Creating dir: " + download_dir
+	os.makedirs(download_dir)
+
+# Get the list of nist CVE databases and download them
+nist_cve_catalog_url = "https://nvd.nist.gov/download.cfm"
+
+nist_cve_catalog_req = requests.get(nist_cve_catalog_url)
+tree = bsoup(nist_cve_catalog_req.content)
+nist_downloads = tree.findAll('a', href=re.compile(r'http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-.*\.xml'))
+
+for nist_download in nist_downloads:
+	download_url = nist_download.get('href')
+	download_url.replace("http", "https")
+	download_file = download_dir + "/" + posixpath.basename(urlparse.urlsplit(download_url).path)
+	print "Downloading " + download_url
+	
+	download_req = requests.get(download_url, stream=True)
+	if download_req.status_code == 200:
+		with open(download_file, 'wb') as file_handle:
+			for chunk in download_req.iter_content(download_chunk_size):
+				file_handle.write(chunk)
